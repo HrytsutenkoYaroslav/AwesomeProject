2f8da40426ae439499a604b28a5087fce3704613
HrytsutenkoYaroslav <113903765+HrytsutenkoYaroslav@users.noreply.github.com>
2023-05-29 21:29:50 +0300

    upd

--- AwesomeProject/AwesomeProject/App.js
+++ AwesomeProject/AwesomeProject/App.js
@@ -1,21 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { ImageBackground, StyleSheet, View } from 'react-native';
 import RegistrationScreen from "./Screens/RegistrationScreen"
 export default function App() {
   return (
+    <ImageBackground
+      source={require('./PhotoBG.png')}
+      style={styles.backgroundImage}
+    >
     <View style={styles.container}>
   
       <RegistrationScreen />
       <StatusBar style="auto" />
     </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
+    
   },
+  backgroundImage: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
+  },
+  
 });
--- AwesomeProject/AwesomeProject/Screens/RegistrationScreen.jsx
+++ AwesomeProject/AwesomeProject/Screens/RegistrationScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { View,Text, ImageBackground, StyleSheet, TextInput, Button } from 'react-native';
+import { View, Text, Image,  StyleSheet, TextInput, TouchableOpacity, } from 'react-native';
 
 const RegistrationScreen = () => {
   const [name, setName] = useState('');
@@ -31,51 +31,108 @@ const RegistrationScreen = () => {
   };
 
   return (
-    <ImageBackground
-      source={require('../PhotoBG.png')}
-      style={styles.backgroundImage}
-    >
-      <View style={styles.container}>
+    
+      <View style={styles.RegContainer}>
+        <View style={styles.imageContainer}>
+        <Image source={require('../Rectangle22.png')} style={styles.image} />
+      </View>
+      <Text style={styles.titleText}>Реєстрація </Text>
         <TextInput
-          placeholder="Name"
+          placeholder="    Логін"
           value={name}
           onChangeText={handleNameChange}
           style={styles.input}
         />
         <TextInput
-          placeholder="Email"
+          placeholder="    Адреса електронної пошти"
           value={email}
           onChangeText={handleEmailChange}
           style={styles.input}
         />
         <TextInput
-          placeholder="Password"
+          placeholder="    Пароль"
           value={password}
           onChangeText={handlePasswordChange}
           secureTextEntry={true}
           style={styles.input}
         />
-        <Button title="Register" onPress={handleRegistration} />
+        <TouchableOpacity
+        title="Зареэструватися" 
+        onPress={handleRegistration} 
+        style={styles.button}>
+          <Text style={styles.text}>Зареэструватися</Text >
+        </TouchableOpacity>
+
+        <TouchableOpacity  
+         style={styles.loginLink}>
+          <Text >Вже є акаунт? Увійти</Text >
+          </TouchableOpacity>
       </View>
-    </ImageBackground>
+    
   );
 };
 const styles = StyleSheet.create({
-  backgroundImage: {
-    width:100,
+  
+  RegContainer: {
+    backgroundColor: 'rgba(255, 255, 255, 1)',
     flex: 1,
-    resizeMode: 'cover',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    marginTop: 263,
+    borderTopLeftRadius: 25, 
+    borderTopRightRadius: 25,
+    position: 'relative',
   },
-  container: {
-    padding: 20,
+  imageContainer: {
+    position: 'absolute',
+    top: '0%',
+    left: '50%',
+    transform: [{ translateX: -60 }, { translateY: -60 }],
+  },
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 16,
+  },
+  text: {
+
+    color: '#FFFFFF',
+textAlign: "center",
+  },
+  titleText: {
+marginBottom: 17,
+marginTop: 32,
+    color: '#212121',
+textAlign: "center",
   },
   input: {
-    marginBottom: 10,
-    padding: 10,
-    backgroundColor: 'rgba(255, 255, 255, 0.8)',
-    borderRadius: 5,
+    marginLeft: 16,
+    marginRight: 16,
+    height: 50,
+    borderWidth: 1,
+    borderColor: '#E8E8E8',
+    marginTop: 16,
+    backgroundColor: '#F6F6F6',
+    borderRadius: 8,
   },
+  button: {
+    borderRadius: 100,
+    backgroundColor: '#FF6C00',
+    textAlign: "center",
+    justifyContent: "center",
+   marginLeft: 16,
+   marginTop: 43,
+   marginRight: 16,
+   height: 51,
+   
+   
+  },
+  loginLink: {
+    paddingBottom: 78,
+    paddingTop: 16,
+    alignItems: 'center',
+    color: '#1B4371',
+    backgroundColor: '#F6F6F6'
+  }
 });
 
 
